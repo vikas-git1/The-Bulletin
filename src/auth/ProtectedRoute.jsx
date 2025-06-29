5ea93cdedb706b50101868933be60d27a9f869c1
vikas-git1 <vikasmewara.dev@gmail.com>
2025-06-29 12:02:51 +0530

    Create Protected Routes

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = () => {
-  return <div></div>;
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+  return user ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
